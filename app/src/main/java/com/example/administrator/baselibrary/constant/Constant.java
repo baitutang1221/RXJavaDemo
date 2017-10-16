2584315b54831470b5c8ce740c8c7b95ed997502
tfs_dangjiancai <cai0241>
2017-10-16 16:02:37 +0800

    hide website

--- app/src/main/java/com/example/administrator/baselibrary/constant/Constant.java
+++ app/src/main/java/com/example/administrator/baselibrary/constant/Constant.java
@@ -2,7 +2,7 @@ package com.example.administrator.baselibrary.constant;
 
 public class Constant {
 
-    public static final String URL = "http://iss.demo.qikan.com/";//com正式地址
+    public static final String URL = "http://12345.com/";//com正式地址
 
     public static final String WEBURL = URL;
 
